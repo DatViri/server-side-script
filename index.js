3d96a8f69c1c3c8781bc84b09bfb579a9c354041
Dat Truong <dat.truonghuu1212@gmail.com>
2019-04-05 09:36:06 +0300

    add helmet and ejs

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const imageRouter = require('./router/imageRouter');
 const userRouter = require('./router/userRouter');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const helmet = require('helmet');
 
 require('./model/userModel');
 require('dotenv').config();
@@ -23,6 +24,8 @@ app.use(session({
   cookie: {maxAge: 60000},
   resave: false,
   saveUninitialized: false}));
+app.use(helmet());
+app.set('view engine', 'ejs');
 
 // Connect DB
 mongoose.connect(url).then(()=>{
@@ -49,6 +52,6 @@ app.use('/api/users', userRouter);
 // });
 
 app.get('/', (req, res) => {
-  res.sendfile(__dirname + '/public/index.html');
+  res.render('index');
 });
 
